b74de674f7c7cd2720bf6b5337020467e5d79f94
upwardbinkie <upwardbinkie@gmail.com>
2019-12-12 00:02:26 -0600

    Add StaticPages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  # StaticPages
+  get 'home' => 'static_pages#home'
+  get 'about' => 'static_pages#about'
+  get 'faq' => 'static_pages#faq'
+  get 'terms' => 'static_pages#terms'
+  get 'privacy' => 'static_pages#privacy'  
+
+  # You can have the root of your site routed with "root"
+  root 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get static_pages_faq_url
+    assert_response :success
+  end
+
+  test "should get privacy" do
+    get static_pages_privacy_url
+    assert_response :success
+  end
+
+  test "should get terms" do
+    get static_pages_terms_url
+    assert_response :success
+  end
+
+end
